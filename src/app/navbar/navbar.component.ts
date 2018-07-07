2b47a80cd1d13b5da8d96cc52b27089e5032031d
B0ER <qwertygo85@gmail.com>
2018-07-07 16:25:43 +0300

    Add dynamiclly set class to DOM element in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {ShareService} from '../share.service';
 import {Router} from '@angular/router';
 
@@ -8,20 +8,28 @@ import {Router} from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('userLink') userLink: ElementRef;
+  @ViewChild('speakerLink') speakerLink: ElementRef;
+  @ViewChild('questionLink') questionLink: ElementRef;
+  @ViewChild('photoLink') photoLink: ElementRef;
 
-  constructor(private shareService: ShareService, private router: Router) {
+  constructor(private shareService: ShareService, private router: Router, private render: Renderer2) {
   }
 
 
   ngOnInit() {
     switch (this.router.url) {
       case '/global':
+        this.render.addClass(this.userLink.nativeElement, 'active_link');
         break;
       case '/ask':
+        this.render.addClass(this.questionLink.nativeElement, 'active_link');
         break;
       case '/speakers':
+        this.render.addClass(this.speakerLink.nativeElement, 'active_link');
         break;
       case '/photo':
+        this.render.addClass(this.photoLink.nativeElement, 'active_link');
         break;
     }
   }
